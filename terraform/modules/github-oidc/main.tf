6c6aca863678ddd570b9870971e50f1327ea6915
rajdev0ps <raj.deolekar.ed@outlook.com>
2026-08-25 15:41:42 -0500

    fix: update OIDC workflow and module

--- terraform/modules/github-oidc/main.tf
+++ terraform/modules/github-oidc/main.tf
@@ -24,7 +24,7 @@ resource "aws_iam_openid_connect_provider" "github" {
   count = var.create_oidc_provider ? 1 : 0
 
   url             = "https://${local.oidc_host}"
-  client_id_list  = ["sts.amazonaws.com"]
+  client_id_list  = ["sts.amazonaws.com", "https://github.com/rajdev0ps"]
   thumbprint_list = [
     "6938fd4d98bab03faadb97b34396831e3780aea6",
     "1c58a3a8518e8759bf075b76b750d4f2df264fcd",
